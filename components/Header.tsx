fe2a7c691116797ce3a8d284d643fa122a2544d1
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2023-01-08 22:45:00 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
         <div>
           <Link href="/" passHref role="button">
             <img
-              src={`/Homeicon.png`}
+              src={`/icons/Homeicon.png`}
               alt="Home"
               width={40}
               height={40}
