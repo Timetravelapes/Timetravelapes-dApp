cbc157b258c28f707cff0151428515fd6b79e13a
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2023-01-11 13:01:50 +0100

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,14 +1,10 @@
 import HeroSection from "./HeroSection/HeroSection";
-import Model from "./Model/Model";
-import NavBar from "./NavBar/NavBar";
 import SearchToken from "./SearchToken/SearchToken";
 import Toggle from "./Toggle/Toggle";
 import Token from "./Token/Token";
 
 export {
     HeroSection,
-    Model,
-    NavBar,
     SearchToken,
     Toggle,
     Token
