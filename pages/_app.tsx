e06e7550cc78db356c2deca72e3c0a733289a1af
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2023-01-15 10:11:15 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,8 +20,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Use Our Marketplace List Your NFTs For Sale, Accept Bids, and Buy NFTs"
           
         />
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" href="../public/logo.png">
+        <meta  rel="icon" type="image/png" href="../public/logo.png" />
+        
         <meta
           name="keywords"
           content="Timetravelapes, dApp, Marketplace, NFT Marketplace, NFT Auction, OpenSea"
