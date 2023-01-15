97ac7c40f4df2c2860d41bc21c281f1e1e29e702
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2023-01-15 11:03:52 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Use Our Marketplace List Your NFTs For Sale, Accept Bids, and Buy NFTs"
          
         />
-        <meta property="image" content="../public/icons/favicon.ico" />
+        <meta property="og:image" content="../public/icons/favicon.ico" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:image:alt" content="TTA" />
         
         
         <meta
